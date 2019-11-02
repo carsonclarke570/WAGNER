95dfd20bec416844c2897a8fd454c83a1c5285ba
Carson Clarke-Magrab <carsonclarke570@gmail.com>
2019-11-02 16:22:38 -0400

    Add crc

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def example():
-    return "Hello World!"
+    # creates HMAC SHA-256 hash from incomming token and your consumer secret
+  sha256_hash_digest = hmac.new(API_SECRET, msg=request.args.get('crc_token'), digestmod=hashlib.sha256).digest()
+
+  # construct response data with base64 encoded hash
+  response = {
+    'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest)
+  }
+
+  # returns properly formatted json response
+  return json.dumps(response)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
